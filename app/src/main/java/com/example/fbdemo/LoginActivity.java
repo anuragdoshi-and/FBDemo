450a1c78e0cfca45b200ecb79d5f6dadb1e78d56
anurag <anuragdoshi1@gmail.com>
2020-10-24 11:09:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fbdemo/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.fbdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText logEmail;
+    private EditText logPassword;
+    private Button login;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        logEmail = findViewById(R.id.logEmail);
+        logPassword = findViewById(R.id.logPassword);
+        login = findViewById(R.id.login);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = logEmail.getText().toString();
+                String txt_password = logPassword.getText().toString();
+                loginUser(txt_email, txt_password);
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "Logged In!", Toast.LENGTH_SHORT).show();
+                //---------------------------CHANGE REQU-----------------------------------
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/fbdemo/registerActivity.java
@@ -0,0 +1,67 @@
+package com.example.fbdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class registerActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button signup;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+        auth = FirebaseAuth.getInstance();
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if((TextUtils.isEmpty(txt_email)) || (TextUtils.isEmpty(txt_password))){
+                    Toast.makeText(registerActivity.this, "Please Enter Credentials", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    signupUser(txt_email, txt_password);
+                }
+            }
+        });
+
+    }
+
+    private void signupUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(registerActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(registerActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(registerActivity.this,HomeActivity.class));
+                    finish();
+                }
+                else{
+                    Toast.makeText(registerActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
